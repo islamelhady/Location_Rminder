ffcacc1f1155cd37376cb2276f1f382a57e3baac
elhady <i.elhady44@gmail.com>
2023-02-15 14:46:03 +0200

    Updating the Broadcast Receiver

--- app/src/main/java/com/udacity/project4/utils/Extensions.kt
+++ app/src/main/java/com/udacity/project4/utils/Extensions.kt
@@ -12,6 +12,8 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.gms.location.Geofence
+import com.google.android.gms.location.GeofenceStatusCodes
+import com.udacity.project4.R
 import com.udacity.project4.base.BaseRecyclerViewAdapter
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -71,4 +73,20 @@ fun View.fadeOut() {
 object GeofenceConstants {
     const val GEOFENCE_RADIUS_IN_METERS = 100f
     const val NEVER_EXPIRES = Geofence.NEVER_EXPIRE
+}
+
+fun errorMessage(context: Context, errorCode: Int): String {
+    val resources = context.resources
+    return when (errorCode) {
+        GeofenceStatusCodes.GEOFENCE_NOT_AVAILABLE -> resources.getString(
+            R.string.geofence_not_available
+        )
+        GeofenceStatusCodes.GEOFENCE_TOO_MANY_GEOFENCES -> resources.getString(
+            R.string.geofence_too_many_geofences
+        )
+        GeofenceStatusCodes.GEOFENCE_TOO_MANY_PENDING_INTENTS -> resources.getString(
+            R.string.geofence_too_many_pending_intents
+        )
+        else -> resources.getString(R.string.unknown_geofence_error)
+    }
 }
