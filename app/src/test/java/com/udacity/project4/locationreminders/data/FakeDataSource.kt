ae8536874379be8693a7bc5caa28d47d71fea18e
elhady <i.elhady44@gmail.com>
2023-02-23 19:18:15 +0200

    Create a fake data source to replace data layer

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -4,25 +4,44 @@ import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
-class FakeDataSource : ReminderDataSource {
+class FakeDataSource(var reminders: MutableList<ReminderDTO>? = mutableListOf()) :
+    ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
-
+    private var shouldReturnError = false
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (shouldReturnError) {
+            return Result.Error("Error getting reminders")
+        }
+        reminders?.let { return Result.Success(it) }
+        return Result.Error("Reminders not found")
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        reminders?.add(reminder)
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        val reminder = reminders?.find { reminderDTO ->
+            reminderDTO.id == id
+        }
+        return when {
+            reminder != null -> {
+                Result.Success(reminder)
+            }
+            shouldReturnError -> {
+                Result.Error(" Not found reminder!")
+            }
+            else -> {
+                Result.Error("Not found reminder!")
+            }
+        }
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        reminders?.clear()
     }
 
-
+    fun setReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
 }
